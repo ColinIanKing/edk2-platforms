5927f49d50a5b2229e0c362f8c381aa72d1d12c1
Jeremy Linton <jeremy.linton@arm.com>
2021-04-16 13:03:41 -0500

    platforms/rpi: Normal memory should not be marked as efi uncached.
    The efi spec seems to indicate that the efi uncacheable attribute
    should be mapped to device memory rather than normal-nc. This means
    that the uefi mem attribute for the >3G ram doesn't match the remainder
    of the RAM in the machine.
    
    So, lets remove the uncacheable attribute to make it more consistent.
    
    Signed-off-by: Jeremy Linton <jeremy.linton@arm.com>

--- Platform/RaspberryPi/Drivers/ConfigDxe/ConfigDxe.c
+++ Platform/RaspberryPi/Drivers/ConfigDxe/ConfigDxe.c
@@ -500,7 +500,7 @@ ApplyVariables (
 
     Status = gDS->AddMemorySpace (EfiGcdMemoryTypeSystemMemory, 3UL * BASE_1GB,
                     SystemMemorySizeBelow4GB - (3UL * SIZE_1GB),
-                    EFI_MEMORY_UC | EFI_MEMORY_WC | EFI_MEMORY_WT | EFI_MEMORY_WB);
+                    EFI_MEMORY_WC | EFI_MEMORY_WT | EFI_MEMORY_WB);
     ASSERT_EFI_ERROR (Status);
     Status = gDS->SetMemorySpaceAttributes (3UL * BASE_1GB,
                     SystemMemorySizeBelow4GB - (3UL * SIZE_1GB), EFI_MEMORY_WB);
@@ -512,7 +512,7 @@ ApplyVariables (
       //
       Status = gDS->AddMemorySpace (EfiGcdMemoryTypeSystemMemory, 4UL * BASE_1GB,
                       SystemMemorySize - (4UL * SIZE_1GB),
-                      EFI_MEMORY_UC | EFI_MEMORY_WC | EFI_MEMORY_WT | EFI_MEMORY_WB);
+                      EFI_MEMORY_WC | EFI_MEMORY_WT | EFI_MEMORY_WB);
       ASSERT_EFI_ERROR (Status);
       Status = gDS->SetMemorySpaceAttributes (4UL * BASE_1GB,
                       SystemMemorySize - (4UL * SIZE_1GB), EFI_MEMORY_WB);
